b189824208ad2794108e56b666eb22a5aaaab933
Lucas dos Santos Soares <129183068+17071996-Lucas-dos-Santos-Soares@users.noreply.github.com>
2025-05-12 19:33:36 -0300

    landing page para a pizzaria Don Francesco
    landing page feita em bootstrap 5, javascript, e a biblioteca de animação scroll reveal.

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const inputCheck = document.querySelector('#dark-mode')
+const element = document.querySelector('body')
+inputCheck.addEventListener('click',() => {
+    const mode = inputCheck.checked ? 'dark': 'light'
+    element.setAttribute("data-bs-theme", mode)
+})
+function atualizarData() {
+    let data = new Date()
+    let dia = data.getDate()
+    let mes = data.getMonth() + 1
+    let ano = data.getFullYear()
+    let dataFormatada = `${dia}/${mes}/${ano}`
+    document.getElementById('dataAtual').innerHTML = `&copy; Desenvolvido por Lucas Soares ${dataFormatada}`
+}
+window.onload = function() {
+    atualizarData()
+}
+window.revelar = ScrollReveal({reset:true})
+revelar.reveal('.animate-title1',{
+     duration: 2000,
+     distance: '90px',
+     origin: 'left'
+})
+
+window.revelar = ScrollReveal({reset:true})
+revelar.reveal('.animate-paragraph1',{
+     duration: 2000,
+     distance: '90px',
+     origin: 'left'
+})
+
+window.revelar = ScrollReveal({reset:true})
+revelar.reveal('.animate-main-btn',{
+     duration: 2000,
+     distance: '90px',
+     origin: 'left'
+})
