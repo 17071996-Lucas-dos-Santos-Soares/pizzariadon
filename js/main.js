9eb7fc15562a56537e1a87c4769e8a7b7c8d6675
Lucas dos Santos Soares <129183068+17071996-Lucas-dos-Santos-Soares@users.noreply.github.com>
2025-06-06 20:49:40 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -35,3 +35,53 @@ revelar.reveal('.animate-main-btn',{
      distance: '90px',
      origin: 'left'
 })
+// carrinho
+const menu = document.getElementById("menu")
+const cartBtn = document.getElementById("cart-btn")
+const cartModal = document.getElementById("cart-modal")
+const cartItemsContainer = document.getElementById("cart-items")
+const cartTotal = document.getElementById("cart-total")
+const checkoutBtn = document.getElementById("checkout-btn")
+const closeBtn = document.getElementById("close-btn")
+const cartCounter = document.getElementById("cart-count")
+const addressInput = document.getElementById("adress")
+const adressWarn = document.getElementById("adress-warn")
+
+let cart = [];
+// abri o modal do carrinho
+cartBtn.addEventListener("click", function(){
+    cartModal.style.display = "flex"
+})
+// fechar o modal do carrinho
+cartModal.addEventListener("click", function(event){
+    if(event.target === cartModal){
+        cartModal.style.display = "none"
+    }
+})
+
+closeBtn.addEventListener("click", function(){
+    cartModal.style.display = "none"
+})
+
+menu.addEventListener("click", function(event){
+    let parentButton = event.target.closest(".add-to-cart")
+    if(parentButton){
+        const name = parentButton.getAttribute("data-name")
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+        addToCart(name, price)
+    }
+})
+
+// função para adicionar ao carrinho.
+function addToCart(name, price){
+   const existingItem = cart.find(item => item.name)
+   if(existingItem) {
+    existingItem.quantity += 1;
+    return
+   } 
+    cart.push({
+        name,
+        price,
+        quantity: 1,
+    })
+}
